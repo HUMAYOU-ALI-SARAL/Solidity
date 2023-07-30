8de36d768a5f84d27ab5357cb7376c9afe5e1295
HUMAYOU-ALI-SARAL <humayouali63@gmail.com>
2023-07-31 00:11:54 +0500

    Day1

---
+++ contracts/Day1.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT  
+pragma solidity ^0.8.18;          //Specifies Version Of Compiler "^" is used to specify versions, above then 0.8.18
+
+contract Day1{
+    string public name="humayou";
+    uint private age=20;
+    bool public student=true;
+    int public salary=100000;
+    bytes public major="Computer Science";
+}
